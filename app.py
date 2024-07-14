5f5c3130ade6584ec125026d4842a40d846267a0
Gabriel-M3LO <gabrielzulifi@gmail.com>
2024-07-14 04:07:41 -0300

    melhorias no backend

--- app.py
+++ app.py
@@ -1,118 +1,108 @@
-import MySQLdb
-import mysql.connector
 from flask import Flask, render_template, request, redirect, session, url_for, flash
-from flask_mysqldb import MySQL
-from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
 import os
 
-cnx = mysql.connector.connect(
-    user='root',
-    password='root',
-    host='127.0.0.1'
-)
-
 app = Flask(__name__)
-app.config.from_object('config.Config')
+app.secret_key = 'root'
 
-mysql = MySQL(app)
-Session(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+        SGBD='mysql+mysqlconnector',
+        usuario='root',
+        senha='root',
+        servidor='localhost',
+        database='gestao_correta'
+    )
 
+db = SQLAlchemy(app)
+
+
+class users(db.Model):
+    idusers = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(80), nullable=False)
+    departament = db.Column(db.String(80), nullable=False)
+    company = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return '<Usuario {}>'.format(self.username)
+
+
+class files(db.Model):
+    idFiles = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    link = db.Column(db.String(80), nullable=False)
+    fileImage = db.Column(db.String(80), nullable=False)
+    fileDepartment = db.Column(db.String(80), nullable=False)
+    fileCompany = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return '<Arquivo {}>'.format(self.title)
 
 @app.route('/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+def index():
+    return render_template('index.html')
 
-        cur = mysql.connection.cursor()
-        cur.execute(
-            "SELECT idusers, username, email, password, role, company, departament FROM users WHERE email = %s",
-            (email,)
-        )
-        user = cur.fetchone()
-        cur.close()
 
-        if email and password == user[3]:
-            session['user_id'] = user[0]
-            session['name'] = user[1]
-            session['email'] = email[2]
-            session['departamento'] = user[6]
-            session['empresa'] = user[5]
-            session['role'] = user[4]
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    usuario = users.query.filter_by(email=request.form['email']).first()
+    if usuario:
+        if usuario.password == request.form['password']:
+            session['usuario_logado'] = usuario.username
+            session['departament'] = usuario.departament
+            session['username'] = usuario.username
+            session['company'] = usuario.company
+            session['role'] = usuario.role
             return redirect(url_for('dashboard'))
-        else:
-            flash('Email/Senha não encontrados', 'error')
 
     return render_template('index.html')
 
 
 @app.route('/dashboard')
 def dashboard():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
+    if 'usuario_logado' in session:
+        arquivos = []
+        if session['role'] == 'Adm':
+            arquivos = files.query.all()
+        elif session['role'] == 'Gerente de departamento':
+            arquivos = files.query.filter_by(fileDepartment=session['departament'], fileCompany=session['company']).all()
+        elif session['role'] == 'Gestor':
+            user_id = users.query.filter_by(username=session['usuario_logado']).first().idusers
+            arquivos = db.session.query(files).join(filesusers).filter(filesusers.idusers == user_id).all()
+        else:
+            user_id = users.query.filter_by(username=session['usuario_logado']).first().idusers
+            arquivos = db.session.query(files).join(filesusers).filter(filesusers.idusers == user_id).all()
 
-    cur = mysql.connection.cursor()
-    userid = session['user_id']
-    arquivos = []
+        arquivos_por_departamento = {}
+        for arquivo in arquivos:
+            departamento = arquivo.fileDepartment
+            if departamento not in arquivos_por_departamento:
+                arquivos_por_departamento[departamento] = []
+            arquivos_por_departamento[departamento].append(arquivo)
 
-    if session['role'] == 'Administrador':
-        cur.execute("SELECT Title, link, Img, Departamento, Empresa FROM files")
-        arquivos = cur.fetchall()
-    elif session['role'] == 'Gerente de departamento':
-        cur.execute("SELECT Title, link, Img, Departamento, Empresa FROM files WHERE Departamento = %s AND Empresa = %s",
-                    (session['departamento'], session['empresa']))
-        arquivos = cur.fetchall()
-    elif session['role'] == 'Gestor':
-        cur.execute("SELECT idfiles FROM filesusers WHERE idusers = %s", (userid,))
-        dash = cur.fetchall()
+        departamentos = list(arquivos_por_departamento.keys())
+        if departamentos:
+            departamento_name = files.query.filter(files.title.in_(departamentos)).all()
+        else:
+            departamento_name = []
 
-        idfiles_list = [item[0] for item in dash]
-        if idfiles_list:
-            placeholders = ', '.join(['%s'] * len(idfiles_list))
-            query = f"SELECT Title, link, Img, Departamento, Empresa FROM files WHERE idfiles IN ({placeholders})"
-            cur.execute(query, idfiles_list)
-            arquivos = cur.fetchall()
-    else:
-        cur.execute("SELECT idfiles FROM filesusers WHERE idusers = %s", (userid,))
-        dash = cur.fetchall()
+        return render_template('dashboard.html', arquivos_por_departamento=arquivos_por_departamento, departamento_name=departamento_name)
+    return redirect(url_for('index'))
 
-        idfiles_list = [item[0] for item in dash]
-        if idfiles_list:
-            placeholders = ', '.join(['%s'] * len(idfiles_list))
-            query = f"SELECT Title, link, Img, Departamento, Empresa FROM files WHERE idfiles IN ({placeholders})"
-            cur.execute(query, idfiles_list)
-            arquivos = cur.fetchall()
 
-    # Agrupa arquivos por departamento
-    arquivos_por_departamento = {}
-    for arquivo in arquivos:
-        departamento = arquivo[3]  # índice do campo 'Departamento'
-        if departamento not in arquivos_por_departamento:
-            arquivos_por_departamento[departamento] = []
-        arquivos_por_departamento[departamento].append(arquivo)
-
-    # Obter nomes dos departamentos
-    departamentos = list(arquivos_por_departamento.keys())
-    placeholders = ', '.join(['%s'] * len(departamentos))
-    query = f"SELECT nome FROM departamentos WHERE nome IN ({placeholders})"
-    cur.execute(query, departamentos)
-    departamento_name = cur.fetchall()
-
-    cur.close()
-
-    return render_template('dashboard.html', arquivos_por_departamento=arquivos_por_departamento, departamento_name=departamento_name)
-
-# <---LOGOUT--->
 @app.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('login'))
+    return redirect(url_for('index'))
 
 
 @app.route('/cadastrarDep', methods=['POST'])
 def cadastrarDep():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
+    if 'usuario_logado' not in session:
+        return redirect(url_for('index'))
 
     Nome = request.form['Nome']
     link = request.form['Link']
@@ -121,40 +111,35 @@ def cadastrarDep():
     img_filename = Imagem.filename
     Imagem.save(os.path.join('static/img', img_filename))
 
-    cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO Departamentos (Nome, Imagem, link) VALUES (%s, %s, %s)",
-                (Nome, img_filename, link))
-    mysql.connection.commit()
-    cur.close()
+    novo_arquivo = files(title=Nome, link=link, fileImage=img_filename, fileDepartment=session['departament'], fileCompany=session['company'])
+    db.session.add(novo_arquivo)
+    db.session.commit()
 
     return redirect(url_for('dashboard'))
 
 
 @app.route('/cadastrarUser', methods=['POST'])
 def cadastrarUser():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
+    if 'usuario_logado' not in session:
+        return redirect(url_for('index'))
 
     nome = request.form['Nome']
     email = request.form['email']
-    nível = request.files['nivel']
-    empresa = request.files['empresa']
-    departamento = request.files['departamento']
-    senha = request.files['senha']
+    nível = request.form['nivel']
+    empresa = request.form['empresa']
+    departamento = request.form['departamento']
+    senha = request.form['senha']
 
-    cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO users (username, email, role, company, departament, password) VALUES (%s, %s, %s)",
-                (nome, email, nível, empresa, departamento, senha))
-    mysql.connection.commit()
-    cur.close()
+    novo_usuario = users(username=nome, email=email, role=nível, company=empresa, departament=departamento, password=senha)
+    db.session.add(novo_usuario)
+    db.session.commit()
 
     return redirect(url_for('config'))
 
-
 @app.route('/cadastrarFile', methods=['POST'])
 def cadastrarFile():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
+    if 'usuario_logado' not in session:
+        return redirect(url_for('index'))
 
     Nome = request.form['Nome']
     link = request.form['Link']
@@ -165,51 +150,44 @@ def cadastrarFile():
     img_filename = Imagem.filename
     Imagem.save(os.path.join('static/img/dash', img_filename))
 
-    cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO files (Title, img, link, departamento, empresa) VALUES (%s, %s, %s, %s, %s)",
-                (Nome, img_filename, link, departamento, empresa))
-    mysql.connection.commit()
-    cur.close()
+    novo_arquivo = files(title=Nome, link=link, fileImage=img_filename, fileDepartment=departamento, fileCompany=empresa)
+    db.session.add(novo_arquivo)
+    db.session.commit()
 
     return redirect(url_for('dashboard'))
 
 
 @app.route('/config')
 def config():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
+    if 'usuario_logado' not in session:
+        return redirect(url_for('index'))
 
-    cur = mysql.connection.cursor()
     if session['role'] == 'Administrador':
-        cur.execute("SELECT Nome, Imagem, link FROM departamentos")
+        arquivos = files.query.all()
     else:
-        cur.execute(
-            "SELECT Nome, Imagem, link FROM departamentos WHERE Nome = %s",
-            (session['departamento'],)
-        )
-    arquivos = cur.fetchall()
-    cur.close()
+        arquivos = files.query.filter_by(fileDepartment=session['departament'], fileCompany=session['company']).all()
 
-    departamento = session['departamento']
-    empresa = session['empresa']
+    # Definindo 'usuarios' como lista vazia como padrão
+    usuarios = []
 
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    if session['role'] == 'Administrador':
-        cursor.execute('SELECT * FROM users')
+    if session['role'] == 'Adm':
+        usuarios = users.query.all()
     elif session['role'] == 'Gestor':
-        cursor.execute('SELECT * FROM users WHERE company = %s', (empresa, ))
+        usuarios = users.query.filter_by(company=session['company']).all()
     elif session['role'] == 'Gerente de departamento':
-        cursor.execute('SELECT * FROM users WHERE company = %s and departament =%s ', (empresa, departamento,))
+        usuarios = users.query.filter_by(company=session['company'], departament=session['departament']).all()
 
-    usuarios = cursor.fetchall()
+    # Adicionando prints para debug
+    print("Usuários encontrados:", usuarios)
+    print("Sessão:", session)
 
     return render_template('config.html', arquivos=arquivos, usuarios=usuarios, titulo='config')
 
 
 @app.route('/CadastrarUser', methods=['GET', 'POST'])
 def CadastrarUser():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
+    if 'usuario_logado' not in session:
+        return redirect(url_for('index'))
 
     if request.method == 'POST':
         username = request.form['username']
@@ -219,12 +197,9 @@ def CadastrarUser():
         departament = request.form['departament']
         role = request.form['role']
 
-        cur = mysql.connection.cursor()
-        cur.execute(
-            "INSERT INTO users (username, email, password, company, departament, role) VALUES (%s, %s, %s, %s, %s, %s)",
-            (username, email, password, company, departament, role))
-        mysql.connection.commit()
-        cur.close()
+        novo_usuario = users(username=username, email=email, password=password, company=company, departament=departament, role=role)
+        db.session.add(novo_usuario)
+        db.session.commit()
 
         return redirect(url_for('CadastrarUser'))
 
@@ -233,8 +208,8 @@ def CadastrarUser():
 
 @app.route('/CadastrarDashboard', methods=['GET', 'POST'])
 def CadastrarDashboard():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
+    if 'usuario_logado' not in session:
+        return redirect(url_for('index'))
 
     if request.method == 'POST':
         title = request.form['title']
@@ -244,83 +219,63 @@ def CadastrarDashboard():
         department = request.form['department']
         selected_users = request.form.getlist('usuario')
 
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO files (title, link, img, empresa, departamento) VALUES (%s, %s, %s, %s, %s)",
-                    (title, link, image, company, department))
-        mysql.connection.commit()
+        novo_arquivo = files(title=title, link=link, fileImage=image, fileDepartment=department, fileCompany=company)
+        db.session.add(novo_arquivo)
+        db.session.commit()
 
-        file_id = cur.lastrowid
+        file_id = novo_arquivo.idFiles
 
         for usuario_id in selected_users:
-            cur.execute('''INSERT INTO filesusers (idfiles, idusers) VALUES (%s, %s)''',(file_id, usuario_id))
-        mysql.connection.commit()
-        cur.close()
+            association = filesusers(idfiles=file_id, idusers=usuario_id)
+            db.session.add(association)
+        db.session.commit()
 
         return redirect(url_for('CadastrarDashboard'))
 
-    department = session['departamento']
-    company = session['empresa']
-
+    usuarios = []
     if session['role'] == 'Gerente de departamento':
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute("SELECT * FROM users WHERE departament = %s AND company = %s",
-                       (department, company))
+        usuarios = users.query.filter_by(departament=session['departament'], company=session['company']).all()
     elif session['role'] == 'Gestor':
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute("SELECT * FROM users WHERE company = %s",
-                       (company,))
-    elif session['role'] == 'Administrador':
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute("SELECT * FROM users")
-
-    usuarios = cursor.fetchall()
+        usuarios = users.query.filter_by(company=session['company']).all()
+    elif session['role'] == 'Adm':
+        usuarios = users.query.all()
 
     return render_template('cadastrar_dashboard.html', titulo='cadastrarDashboard', usuarios=usuarios)
 
 
 @app.route('/excluir/<int:usuario_id>', methods=['POST'])
 def excluirUsuario(usuario_id):
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
+    if 'usuario_logado' not in session:
+        return redirect(url_for('index'))
 
-    cur = mysql.connection.cursor()
-    cur.execute("DELETE FROM users WHERE idusers = %s", (usuario_id,))
-    mysql.connection.commit()
-    cur.close()
+    usuario = users.query.get(usuario_id)
+    db.session.delete(usuario)
+    db.session.commit()
     flash('Usuário excluído com sucesso!', 'success')
     return redirect(url_for('config'))
 
 
 @app.route('/editar_usuario/<int:id>', methods=['GET', 'POST'])
 def editar_usuario(id):
-    cur = mysql.connection.cursor()
+    usuario = users.query.get(id)
 
     if request.method == 'POST':
-        nome = request.form['username']
-        email = request.form['email']
-        empresa = request.form['company']
-        departamento = request.form['departament']
-        role = request.form['role']
+        usuario.username = request.form['username']
+        usuario.email = request.form['email']
+        usuario.company = request.form['company']
+        usuario.departament = request.form['departament']
+        usuario.role = request.form['role']
 
-        # Atualiza os dados do usuário no banco de dados
-        cur.execute("""
-            UPDATE users
-            SET username = %s, email = %s, company = %s, departament = %s, role = %s
-            WHERE idusers = %s
-        """, (nome, email, empresa, departamento, role, id))
-
-        mysql.connection.commit()
-        cur.close()
+        db.session.commit()
 
         return redirect(url_for('config'))
-
-    # Obtém os dados atuais do usuário para preencher o formulário
-    cur.execute("SELECT * FROM users WHERE idusers = %s", (id,))
-    usuario = cur.fetchone()
-    cur.close()
 
     return render_template('editar_usuario.html', usuario=usuario)
 
 
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
+
